fc79b4b6f993b50d5299d20f5bc6fb332380c528
Ilya Portnov <portnov@iportnov.ru>
2017-05-28 19:37:05 +0500

    Remove some unused imports.

--- src/System/Log/Heavy.hs
+++ src/System/Log/Heavy.hs
@@ -2,21 +2,7 @@
 
 module System.Log.Heavy where
 
-import Control.Applicative
-import Control.Monad.Reader
-import Control.Monad.Base
-import Control.Monad.Logger (MonadLogger (..), LogLevel (..))
-import Control.Monad.Trans.Control
-import Control.Exception
-import Data.String
-import Data.Monoid
-import Data.List
-import Language.Haskell.TH
-import qualified Data.ByteString as B
-import qualified Data.ByteString.Unsafe as BSU
-import qualified Data.Text as T
-import System.Log.FastLogger as F
-import qualified System.Posix.Syslog as Syslog
+import Control.Monad.Trans
 
 import System.Log.Heavy.Types
 import System.Log.Heavy.Format
