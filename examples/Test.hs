55133e6a8e85cb83760e9dcfa7793cbe411f294a
Ilya Portnov <portnov@iportnov.ru>
2017-10-24 08:53:30 +0500

    Remove unused imports from examples.

--- examples/Test.hs
+++ examples/Test.hs
@@ -1,14 +1,10 @@
 {-# LANGUAGE OverloadedStrings, TypeSynonymInstances, FlexibleInstances, ExistentialQuantification, TypeFamilies, GeneralizedNewtypeDeriving, StandaloneDeriving, FlexibleContexts #-}
 
 import Control.Monad.Trans
-import Control.Monad.Reader
-import Data.Monoid
 import System.Environment
 import System.IO
-import qualified System.Posix.Syslog as Syslog
 import System.Log.Heavy
 import System.Log.Heavy.Shortcuts
-import System.Log.FastLogger
 import Data.Text.Format.Heavy
 import qualified Data.Text.Format.Heavy.Parse as PF
 
